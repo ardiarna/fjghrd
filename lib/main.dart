f5b9c029764fb38a7dc01d43b20b526aea31341f
ardiarna <ardiarna17@gmail.com>
2026-09-18 11:39:41 +0700

    chore: remove screen retriever and window manager plugins from project

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,12 @@ import 'package:fjghrd/views/home_view.dart';
 import 'package:fjghrd/views/karyawan_form.dart';
 import 'package:fjghrd/views/karyawan_payroll_view.dart';
 import 'package:fjghrd/views/login_view.dart';
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  if (!kIsWeb && (Platform.isWindows || Platform.isMacOS || Platform.isLinux)) {
-    await windowManager.ensureInitialized();
-    WindowOptions windowOptions = const WindowOptions(
-      center: true,
-      title: 'HRD FRATEKINDO - Your Partner',
-    );
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-      await windowManager.maximize();
-    });
-  }
 
   final AuthControl authControl = Get.put(AuthControl(), permanent: true);
   await authControl.getSession();
